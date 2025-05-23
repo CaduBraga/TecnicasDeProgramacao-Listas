91c45117204d95c376f58778c0d8346fa4180c07
Cadu Braga <carlos_e_braga@estudante.sesisenai.org.br>
2025-05-23 14:42:22 -0300

    Refactor: arrumando heranças

--- br/com/lista_list/views/ViewAluno.java
+++ br/com/lista_list/views/ViewAluno.java
@@ -1,17 +1,22 @@
-public class ViewAluno {
+package br.com.lista_list.views;
+
+import java.util.Scanner;
+
+import br.com.lista_list.model.Aluno;
+
+public class ViewAluno extends Aluno{
     
     public void preencherDados(Scanner scanner) {
-		System.out.print("Nome: ");
-		this.nome = scanner.nextLine();
-		System.out.print("Idade (anos): ");
-		this.idade = scanner.nextInt();
-		scanner.nextLine();
-		System.out.print("Matrícula: ");
-		this.matricula = scanner.nextLine();
-		System.out.print("Ano de ingresso: ");
-		this.anoIngresso = scanner.nextInt();
-		scanner.nextLine();
-	}
+        System.out.print("Nome: ");
+        setNome(scanner.nextLine());
+        System.out.print("Idade (anos): ");
+        setIdade(scanner.nextInt());
+        System.out.print("Matrícula: ");
+        setMatricula(scanner.nextLine());
+        System.out.print("Ano de ingresso: ");
+        setAnoIngresso(scanner.nextInt());
+        scanner.nextLine();
+    }
 
 	public void imprimirDados() {
 		System.out.println("Informações do Aluno:");
